7ba86a52111ece0a1c1c95c0df5dff007e40f7ed
Jay Depala <jay.depala@valuelabs.com>
2023-03-08 17:09:02 +0530

    first commit

---
+++ src/altusgroup-root-config.ts
@@ -0,0 +1,38 @@
+import { registerApplication, start, LifeCycles } from "single-spa";
+// import { registerApplication, start } from "single-spa";
+import * as isActive from "./activity-functions";
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+registerApplication(
+  "@altusgroup/navbar",
+  () => System.import<LifeCycles>("@altusgroup/navbar"),
+  isActive.nav,
+  { domElement: document.getElementById('nav-container') }
+);
+
+registerApplication(
+  "@altusgroup/page1",
+  () => System.import<LifeCycles>("@altusgroup/page1"),
+  isActive.page1,
+  { domElement: document.getElementById('page-1-container') }
+);
+
+registerApplication(
+  "@altusgroup/page2",
+  () => System.import<LifeCycles>("@altusgroup/page2"),
+  isActive.page2,
+  { domElement: document.getElementById('page-2-container') }
+);
+
+// start({
+//   urlRerouteOnly: true,
+// });
+start();

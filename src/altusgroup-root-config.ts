d2f6470302d95b00684532e68434bd234a88c034
Jay Depala <jay.depala@valuelabs.com>
2023-03-09 11:00:50 +0530

    undo

--- src/altusgroup-root-config.ts
+++ src/altusgroup-root-config.ts
@@ -11,28 +11,28 @@ registerApplication({
   activeWhen: ["/"],
 });
 
-registerApplication(
-  "@altusgroup/navbar",
-  () => System.import<LifeCycles>("@altusgroup/navbar"),
-  isActive.nav,
-  { domElement: document.getElementById('nav-container') }
-);
+// registerApplication(
+//   "@altusgroup/navbar",
+//   () => System.import<LifeCycles>("@altusgroup/navbar"),
+//   isActive.nav,
+//   { domElement: document.getElementById('nav-container') }
+// );
 
-registerApplication(
-  "@altusgroup/page1",
-  () => System.import<LifeCycles>("@altusgroup/page1"),
-  isActive.page1,
-  { domElement: document.getElementById('page-1-container') }
-);
+// registerApplication(
+//   "@altusgroup/page1",
+//   () => System.import<LifeCycles>("@altusgroup/page1"),
+//   isActive.page1,
+//   { domElement: document.getElementById('page-1-container') }
+// );
 
-registerApplication(
-  "@altusgroup/page2",
-  () => System.import<LifeCycles>("@altusgroup/page2"),
-  isActive.page2,
-  { domElement: document.getElementById('page-2-container') }
-);
+// registerApplication(
+//   "@altusgroup/page2",
+//   () => System.import<LifeCycles>("@altusgroup/page2"),
+//   isActive.page2,
+//   { domElement: document.getElementById('page-2-container') }
+// );
 
-// start({
-//   urlRerouteOnly: true,
-// });
-start();
+start({
+  urlRerouteOnly: true,
+});
+// start();

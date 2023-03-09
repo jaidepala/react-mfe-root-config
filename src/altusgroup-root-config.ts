01ddaa2653786f979cb44bcd3788c641fa7698cf
Jay Depala <jay.depala@valuelabs.com>
2023-03-09 10:53:41 +0530

    register only 1 app

--- src/altusgroup-root-config.ts
+++ src/altusgroup-root-config.ts
@@ -2,14 +2,14 @@ import { registerApplication, start, LifeCycles } from "single-spa";
 // import { registerApplication, start } from "single-spa";
 import * as isActive from "./activity-functions";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import<LifeCycles>(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import<LifeCycles>(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
 
 registerApplication(
   "@altusgroup/navbar",
